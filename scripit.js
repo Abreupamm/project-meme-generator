f4910263e9187343ea77041448dd5302fae252c6
pamela <abreupamm@gmail.com>
2022-03-25 02:45:55 -0300

    qustão 2 feita

--- scripit.js
+++ scripit.js
@@ -1,9 +1,18 @@
 const inputText = document.querySelector('#text-input');
 const txt = document.getElementById('meme-text');
-
+const uplImg = document.getElementById('meme-insert');
+let img = document.getElementById('meme-image');
+const reader = new FileReader();
 function criaTexto() {
   const text = inputText.value;
   txt.innerHTML = text;
 }
+function criaImg() {
+  reader.readAsDataURL(uplImg.files[0]);
+  reader.onload = function () {
+    img.src = reader.result;
+  };
+}
 
+uplImg.addEventListener('change', criaImg);
 inputText.addEventListener('input', criaTexto);
